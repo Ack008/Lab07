d75b45949895cbe9426a7cddc02fc3de9c532bb1
ss <francesco.sacripante@studio.unibo.it>
2024-11-05 15:40:44 +0100

    First attempt to implementing IterableWithPolicy

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,31 +1,52 @@
 package it.unibo.inner.impl;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import it.unibo.inner.api.*;
 public class IterableWithPolicyImpl <T> implements IterableWithPolicy<T>{
     private final T[] elements;
+    private Predicate<T> filter;
     public IterableWithPolicyImpl(final T[] array){
+        this(array, new Predicate<T>() {
+            public boolean test(T elem) {
+                return true;
+            }
+        });
+    }
+
+    public IterableWithPolicyImpl(final T[] array, Predicate<T> predicate){
         elements = array.clone();
+        setIterationPolicy(predicate);
     }
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        
+        this.filter = filter;
     }
 
     @Override
     public Iterator<T> iterator() {
         class ArrayIterator implements Iterator<T>{
-            private int arrayPosition = 0;
+            private List<T> filteredArrayList;
+
+            ArrayIterator(){
+                filteredArrayList = new LinkedList<>();
+                for(var elem : elements){
+                    if (filter.test(elem)){
+                        filteredArrayList.add(elem);
+                    }
+                }
+            }
+
             @Override
             public boolean hasNext() {
-                return arrayPosition < elements.length;
+                return !filteredArrayList.isEmpty();
             }
 
             @Override
             public T next() {
-                arrayPosition++;
-                return elements[arrayPosition - 1];
+                return filteredArrayList.removeFirst();
             }
             
         }
