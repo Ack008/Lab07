a98b5ddfd924f5f0878822ca4e004f8e20105d44
ss <francesco.sacripante@studio.unibo.it>
2024-11-05 15:01:03 +0100

    Finiscing the IteratorWithoutPolicy

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,10 +1,38 @@
 package it.unibo.inner.impl;
+import java.util.Iterator;
+
 import it.unibo.inner.api.*;
 public class IterableWithPolicyImpl <T> implements IterableWithPolicy<T>{
+    private final T[] elements;
+    IterableWithPolicyImpl(final T[] array){
+        elements = array.clone();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        class ArrayIterator implements Iterator<T>{
+            private int arrayPosition = 0;
+            @Override
+            public boolean hasNext() {
+                return arrayPosition < elements.length;
+            }
+
+            @Override
+            public T next() {
+                arrayPosition++;
+                return elements[arrayPosition - 1];
+            }
+            
+        }
+        return new ArrayIterator();
+    }
 
     
-    @Override
-    void setIterationPolicy(Predicate<T> filter){
-
-    }
+   
 }
