8cc5b8fa0d7274ec397f6ad7fb19cb91040df10f
ss <francesco.sacripante@studio.unibo.it>
2024-11-05 14:49:27 +0100

    Creation of the implementation of iterable with policy

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,10 @@
+package it.unibo.inner.impl;
+import it.unibo.inner.api.*;
+public class IterableWithPolicyImpl <T> implements IterableWithPolicy<T>{
+
+    
+    @Override
+    void setIterationPolicy(Predicate<T> filter){
+
+    }
+}
